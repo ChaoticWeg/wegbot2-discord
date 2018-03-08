b0c4ea1df1bc2fc7b1697a7875a8b7b62445b80e
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-08 17:41:04 -0600

    linting

--- run.py
+++ run.py
@@ -1,18 +1,26 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import discord
-import asyncio
 import os
+import discord
 
-client = discord.Client()
+from dotenv import load_dotenv
 
-@client.event
-async def on_ready():
-    print('Logged in as {} ({})'.format(client.user.name, client.user.id))
+def run():
+    load_dotenv()
 
-@client.event
-async def on_message(message):
-    print('message')
+    client = discord.Client()
 
-client.run(os.getenv('DISCORD_TOKEN'))
+    @client.event
+    async def on_ready():
+        print('connected as {}'.format(client.user.name))
+
+    @client.event
+    async def on_message(message):
+        print('message in {}'.format(str(message.channel)))
+
+        if message.content.startswith(';dump'):
+            args = message.split(' ')[1:]
+            if not args:
+                print('no args to ;dump')
+            else:
+                print(';dump {}'.format(args[0]))
+
+    client.run(os.getenv('DISCORD_TOKEN'))
