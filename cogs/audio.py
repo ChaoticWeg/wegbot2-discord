580fe724dcbe6e27b5fc3ffe071d60fc01ddf3da
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-08 20:43:07 -0600

    rename cogs.voice to cogs.audio

---
+++ cogs/audio.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class AudioEntry:
+    def __init__(self, message):
+        self.requester = message.author
+        self.channel = message.channel
+
+class AudioState:
+    def __init__(self):
+        pass
+
+class Audio:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(hidden=False)
+    async def summon(self, ctx):
+        new_channel = ctx.message.author.voice_channel
+
+def setup(bot):
+    if not discord.opus.is_loaded():
+        discord.opus.load_opus('opus')
+    bot.add_cog(Audio(bot))
