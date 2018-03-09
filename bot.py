b5055b637fe2980fcc4cdbe4f5f0c2880efb3935
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-08 19:27:55 -0600

    move bot code to bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+import os
+from discord.ext import commands
+
+def _prefix_callable(bot, msg):
+    user_id = bot.user.id
+    result = [ f'<@!{user_id}> ', f'<@{user_id}> ', ';' ]
+    return result
+
+EXTENSIONS = {
+    'cogs.voice'
+}
+
+class Wegbot(commands.AutoShardedBot):
+    def __init__(self):
+        super().__init__(command_prefix=_prefix_callable, description="Wegbot")
+
+        self.token = os.getenv('DISCORD_TOKEN')
+
+        for extension in EXTENSIONS:
+            try:
+                print(f'loading extension {extension}... ', end='')
+                self.load_extension(extension)
+            except:
+                print('FAILED')
+            else:
+                print('OK')
+
+    async def on_ready(self):
+        print(f'logged in as {self.user}')
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        try:
+            await self.process_commands(message)
+        except commands.errors.MissingRequiredArgument:
+            self.send_message(message.channel, 'Missing a required argument. Use ;help for help.')
+
+    def run(self):
+        super().run(self.token)
--- run.py
+++ run.py
@@ -1,26 +1,11 @@
-import os
-import discord
-
 from dotenv import load_dotenv
+from bot import Wegbot
 
 def run():
     load_dotenv()
 
-    client = discord.Client()
+    bot = Wegbot()
+    bot.run()
 
-    @client.event
-    async def on_ready():
-        print('connected as {}'.format(client.user.name))
-
-    @client.event
-    async def on_message(message):
-        print('message in {}'.format(str(message.channel)))
-
-        if message.content.startswith(';dump'):
-            args = message.split(' ')[1:]
-            if not args:
-                print('no args to ;dump')
-            else:
-                print(';dump {}'.format(args[0]))
-
-    client.run(os.getenv('DISCORD_TOKEN'))
+if __name__ == '__main__':
+    run()
