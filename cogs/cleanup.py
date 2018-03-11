417c8b14633450df4eb52d2590de8a3afbb81e3b
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-11 01:40:00 -0600

    purge

--- cogs/cleanup.py
+++ cogs/cleanup.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timedelta
 import os
+
+import discord
 from discord.ext import commands
 
 class Cleanup:
@@ -9,17 +12,55 @@ class Cleanup:
 
     @commands.command(hidden=True)
     async def purge(self, ctx):
-        """ Purge the channel """
+        """
+        Purge the channel.
+        ** Unfortunately, due to Discord API limitations, only messages from within the last 14 days can be purged. Because fuck you."""
 
-        if not ctx.author.id == os.getenv('DISCORD_OWNER'):
+        if not str(ctx.author.id) == os.getenv('DISCORD_OWNER'):
             await ctx.send(f"You don't have permission to purge this channel, {ctx.author.mention}.")
+            print(f"{str(ctx.author)} tried to purge #{str(ctx.channel)}.")
+            print(f"comparing user id ({type(ctx.author.id)}) with env ({type(os.getenv('DISCORD_OWNER'))})")
             return
 
-        if not ctx.channel.id == os.getenv('DISCORD_HOME_CHANNEL'):
+        if not str(ctx.channel.id) == os.getenv('DISCORD_HOME_CHANNEL'):
             await ctx.send(f"I can't purge this channel, {ctx.author.mention}.")
             return
 
-        await ctx.channel.purge()
+        try:
+
+            async with ctx.channel.typing():
+                now = datetime.utcnow()
+                earliest = now - timedelta(days=14)
+
+                print('getting message history. this may take a while...')
+                messages = await ctx.channel.history(limit=None, after=earliest).flatten()
+
+                orig_num_messages = len(messages)
+
+                print(f'purging {orig_num_messages} messages from #{str(ctx.channel)}. this may take a while...')
+
+                while len(messages) > 0:
+                    # delete the first 100 messages if we need to, otherwise just delete them all
+                    temp_messages = messages[:100] if len(messages) > 100 else messages
+                    await ctx.channel.delete_messages(temp_messages)
+
+                    # chop off the first 100 messages if we need to. otherwise, we're done
+                    messages = messages[100:] if len(messages) > 100 else []
+
+                await ctx.send(f"Purged {orig_num_messages} messages from {ctx.channel.mention} at the behest of {ctx.author.mention}.")
+
+        except discord.ClientException:
+            blame = ctx.bot.get_user(int(os.getenv('DISCORD_OWNER'))).mention
+            await ctx.send(f"Tried to purge too many messages at once! {blame} fucked up.")
+
+        except discord.Forbidden:
+            await ctx.send(f"I don't have permission to purge this channel, {ctx.author.mention}.")
+
+        except discord.HTTPException as ex:
+            await ctx.send("Unable to purge this channel. Check the logs.")
+            print(f'*** caught an HTTPException while trying to purge #{str(ctx.channel)}')
+            print(ex)
+
 
 def setup(bot):
     bot.add_cog(Cleanup(bot))
