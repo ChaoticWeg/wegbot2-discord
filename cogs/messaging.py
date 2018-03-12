4b3b6b77b674b444600af694d0af0b59fa224f7d
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-11 23:16:50 -0500

    use WegbotException

--- cogs/messaging.py
+++ cogs/messaging.py
@@ -4,10 +4,7 @@ import os
 import discord
 from discord.ext import commands
 
-class PinException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
+from .errors import WegbotException
 
 class Messaging:
     """ Message-related commands """
@@ -27,20 +24,20 @@ class Messaging:
             messages = [ msg for msg in messages if msg.id in message_ids ]
 
             if not message_ids:
-                raise PinException(f"{target_user.name} hasn't said anything recently")
+                raise WegbotException(f"{target_user.name} hasn't said anything recently")
 
             message = messages[0]
 
             if message.author == ctx.author:
-                raise PinException("You can't use me to pin your own comment")
+                raise WegbotException("You can't use me to pin your own comment")
             
             if message.author == self.bot.user:
-                raise PinException("I won't pin my own message")
+                raise WegbotException("I won't pin my own message")
 
             await message.pin()
 
-        except PinException as ex:
-            await ctx.send(f"{ex}, {ctx.author.mention}.")
+        except WegbotException as ex:
+            await ctx.send(f"{ex.message}, {ctx.author.mention}.")
 
         except Exception as ex:
             print(ex)
@@ -53,22 +50,20 @@ class Messaging:
 
         ** Unfortunately, due to Discord API limitations, only messages from within the last 14 days can be purged. Because fuck you."""
 
-        if not str(ctx.channel.id) == os.getenv('DISCORD_HOME_CHANNEL'):
-            await ctx.send(f"I can't purge this channel, {ctx.author.mention}.")
-            return
-
         try:
+
+            if not str(ctx.channel.id) == os.getenv('DISCORD_HOME_CHANNEL'):
+                raise WegbotException("I can't purge this channel")
 
             async with ctx.typing():
                 now = datetime.utcnow()
                 earliest = now - timedelta(days=14)
 
-                print('getting message history. this may take a while...')
                 messages = await ctx.channel.history(limit=None, after=earliest).flatten()
 
                 orig_num_messages = len(messages)
 
-                print(f'purging {orig_num_messages} messages from #{str(ctx.channel)}. this may take a while...')
+                print(f'purging {orig_num_messages} messages from #{str(ctx.channel)}... ', end='')
 
                 while messages:
                     # delete the first 100 messages if we need to, otherwise just delete them all
@@ -81,16 +76,24 @@ class Messaging:
                 await ctx.send(f"Purged {orig_num_messages} messages from {ctx.channel.mention} at the behest of {ctx.author.mention}.")
 
         except discord.ClientException:
+            print('TOO MANY')
             blame = ctx.bot.get_user(int(os.getenv('DISCORD_OWNER'))).mention
             await ctx.send(f"Tried to purge too many messages at once! {blame} fucked up.")
 
         except discord.Forbidden:
+            print('FORBIDDEN')
             await ctx.send(f"I don't have permission to purge this channel, {ctx.author.mention}.")
 
         except discord.HTTPException as ex:
-            await ctx.send("Unable to purge this channel. Check the logs.")
+            print('FAILED')
+            await ctx.send("Something went wrong. Check the logs.")
             print(f'*** caught an HTTPException while trying to purge #{str(ctx.channel)}')
             print(ex)
+        
+        except Exception as ex:
+            print('OWNED ONLINE')
+            await ctx.send("Purge failed spectacularly. Check the logs.")
+            raise ex
 
 
 def setup(bot):
