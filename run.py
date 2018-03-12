656b7ff8053c4baeb04039391c6ccb6fc1c5b2c9
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-11 23:49:24 -0500

    remove defunct signal capture

--- run.py
+++ run.py
@@ -1,19 +1,10 @@
-import signal
-
 from dotenv import load_dotenv
 from bot import Wegbot
-
-async def handle_sigint(bot):
-    await bot.logout()
 
 def run():
     load_dotenv()
 
     bot = Wegbot()
-
-    # capture sigint
-    signal.signal(signal.SIGINT, lambda signal, frame: handle_sigint(bot))
-
     bot.run()
 
 if __name__ == '__main__':
