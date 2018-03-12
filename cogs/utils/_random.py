9af964958a0afa24ae6048caf7fb1991197ecb3c
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-11 23:59:10 -0500

    remove some goodbyes

--- cogs/utils/_random.py
+++ cogs/utils/_random.py
@@ -1,41 +1,28 @@
-import random
+""" Randomized stuff """
 
-IS_SEEDED = False
+import random
+random.seed()
 
 GOODBYES = [
-    'Later!',
-    'Peace.',
     'afk',
     'afk, tornado',
     'brb server on fire',
-    'brb cat on fire',
     '`ACKNOWLEDGEMENT AND ACCEPTANCE OF TERMS`',
     '```\n*** ChaoticWeg sets mode: +b wegbot*!*@*.*\nwegbot has been kicked by ChaoticWeg ( )\n```',
     'YOU ALL SUCK DICK\ner.\nbye.',
-    "/me is away - gone, if anyone talks in the next 25 minutes as me it's weg being an asshole -",
-    'IRC is just multiplayer notepad.',
+    "`*** wegbot is away - gone, if anyone talks in the next 25 minutes as me it's weg being an asshole -`",
     'wait, shit',
-    'yaYEEEET',
-    'Adiós.',
-    'Au revoir!',
-    'Shutting down...',
-    'Whoops',
     "fuck you i didn't touch the cookie, bitch",
-    '/me has quit IRC (r`heaven)',
-    '/me takes a bow',
-    '/me is now known as wegbotWITHGIRLFRIENDWHOISHOT',
+    '`*** wegbot has quit IRC (r`heaven)`',
+    '`*** wegbot is now known as wegbotWITHGIRLFRIENDWHOISHOT`',
     'dammit',
     "Ok I'm back.",
     "`*** Quits: wegbot (No route to host)`",
-    "`wegbot has quit IRC. (Quit)`"
 ]
 
-def _check_seed():
-    global IS_SEEDED
-    if not IS_SEEDED:
-        random.seed()
-
 def goodbye():
+    # pylint: disable=W0603
+    # shut the FUCK UP ABOUT IT
     global GOODBYES
     idx = random.randint(0, len(GOODBYES)-1)
     return GOODBYES[idx]
