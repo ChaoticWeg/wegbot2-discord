061b6f0f4a52de549740312760644505b0f7c74c
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-12 23:19:49 -0500

    fuck my life. just fuck it.

--- cogs/audio/audio.py
+++ cogs/audio/audio.py
@@ -1,16 +1,15 @@
-import asyncio
 import discord
 from discord.ext import commands
 
 from ..errors import WegbotException
-from .clips import Clips
 
 
 class AudioRequest:
     """ Audio request """
-    def __init__(self, channel, clip):
+    def __init__(self, channel, clip, msg):
         self.channel = channel
         self.clip = clip
+        self.msg = msg
 
 
 class Audio:
@@ -60,35 +59,14 @@ class Audio:
         self.voice = None
 
 
-    async def play_request(self, req):
-        """ Attempt to play a request """
-
-        await self.connect_voice(req.channel)
-
-        self.is_playing = True
-
-        try:
-            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(req.clip.path))
-            await self.voice.play(source, after=lambda e: print(f"Player error: {e}") if e else None)
-        except Exception as ex:
-            raise ex
-
-        # if no more requests, done playing
-        try:
-            next_request = self.requests[0]
-        except:
-            self.is_playing = False
-            await self.disconnect_voice()
+    @commands.command(hidden=True, name='requests')
+    @commands.guild_only()
+    async def say_requests(self, ctx):
+        """ Shows queued requests """
+        if not self.requests:
+            await ctx.send(f"No requests, {ctx.author.mention}! Queue one up with `;play`.")
             return
-
-        # remove the request we just pulled
-        try:
-            self.requests = self.requests[1:]
-        except:
-            self.requests = []
-
-        # play the request we just pulled
-        await self.play_request(next_request)
+        await ctx.send(f"{ctx.author.mention} – I have {len(self.requests)} queued up right now!")
 
 
     @commands.command(hidden=True)
@@ -106,44 +84,35 @@ class Audio:
         return True
 
 
+    async def force_dismiss(self, ctx):
+        if ctx.voice_client is None:
+            raise WegbotException()
+        await ctx.voice_client.disconnect()
+
+
     @commands.command(hidden=True)
     @commands.guild_only()
     async def dismiss(self, ctx):
         """ Dismiss the bot from its current voice channel """
 
         if self.voice is None:
-            await ctx.send(f"I'm not in a voice channel, {ctx.author.mention}.")
-            return False
+            try:
+                await self.force_dismiss(ctx)
+            except WegbotException:
+                await ctx.send(f"I'm not in a voice channel, {ctx.author.mention}.")
+                return False
+            else:
+                return True
+        else:
+            await self.disconnect_voice()
+            return True
 
-        await self.disconnect_voice()
-        return True
 
-
-    @commands.command(hidden=False, brief="Play an audio clip.")
+    @commands.command(hidden=False, brief="Play an audio clip.", disabled=True)
     @commands.guild_only()
     async def play(self, ctx, *, clip: str):
         """ Play an audio clip. You must be in a voice channel. """
-
-        try:
-
-            channel = self.get_voice_channel(ctx)
-
-            if channel is None:
-                raise WegbotException("You're not in a voice channel")
-
-            clip = Clips.get_by_name(clip)
-
-            if clip is None:
-                raise WegbotException("No such clip")
-
-            await self.play_request(AudioRequest(channel, clip))
-
-        except WegbotException as ex:
-            await ctx.send(f"{ex.message}, {ctx.author.mention}.")
-
-        except Exception as ex:
-            await ctx.send(f"Error while playing `{clip}`: {ex}")
-            await self.disconnect_voice()
+        pass
 
 
 def setup(bot):
